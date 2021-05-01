b9135864d38b20a1d78d5e2f0cb5f31f3e88a1fc
DAC <dac@cogsmith.com>
2021-05-01 02:20:10 -0400

    .

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ const forge = require('node-forge'); forge.options.usePureJavaScript = true;
 
 //const XT = require('/DEV/CODE/xtdev/node_modules/@cogsmith/xt').Init();
 const XT = require('@cogsmith/xt').Init();
-const LOG = XT.Log;
-const App = XT.App;
+const App = XT.App; const LOG = XT.LOG;
 
 //
 
